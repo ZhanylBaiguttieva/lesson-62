743f99750c3e2259a71573e9793a1dcd2ff6d753
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-27 01:34:35 +0600

    Added css style in apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 
 import './App.css';
+import Toolbar from './containers/Toolbar';
+import {Route, Routes} from 'react-router-dom';
+import Home from './containers/Home';
+import Contacts from './containers/Contacts';
 
 function App() {
 
   return (
     <>
-
+      <header>
+        <Toolbar/>
+      </header>
+      <Routes>
+        <Route path="/" element={(
+          <Home />
+        )}/>
+        <Route path="/contacts" element={(
+          <Contacts />
+        )}/>
+        {/*<Route path="*" element={<h1>Not Found!</h1>}/>*/}
+      </Routes>
     </>
   );
 }
