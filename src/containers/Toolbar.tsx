490d68ba2ae5c9a68c0958092d4ebd35305f0616
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-11-27 01:33:58 +0600

    Added Toolbar.tsx

---
+++ src/containers/Toolbar.tsx
@@ -0,0 +1,22 @@
+import {NavLink} from 'react-router-dom';
+import React from 'react';
+
+const Toolbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">БГП № 11</span>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Главная</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contacts">Контакты</NavLink>
+            </li>
+          </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
